6d13a743cbdfbbcf03414703bca3d3951024409b
Samadi van Koten <samadi@vktec.org.uk>
2019-10-01 17:37:36 +0100

    Remove dependency on Fabric API

--- src/main/java/uk/org/vktec/butterfly/mixin/CommandManagerMixin.java
+++ src/main/java/uk/org/vktec/butterfly/mixin/CommandManagerMixin.java
@@ -1,15 +1,14 @@
 package uk.org.vktec.butterfly.mixin;
 
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.server.command.CommandManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftClient.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
+@Mixin(CommandManager.class)
+public abstract class CommandManagerMixin {
+	@Inject(method = "<init>", at = @At("RETURN"))
 	private void init(CallbackInfo info) {
-		System.out.println("This line is printed by an example mod mixin!");
 	}
 }
